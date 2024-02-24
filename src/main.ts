6cee5e97b98caafc986b9275b7db5c7bbded5f52
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-24 16:59:07 -0300

    chores: initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  });
+}
+bootstrap();
