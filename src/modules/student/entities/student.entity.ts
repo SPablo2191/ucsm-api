040ca13d8b1e370893100af3f747ceb680ff6e98
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-25 18:20:18 -0300

    fix: default value for middle name and second last name

--- src/modules/student/entities/student.entity.ts
+++ src/modules/student/entities/student.entity.ts
@@ -6,19 +6,19 @@ export class Student {
   id: number;
   @Column({ type: 'varchar', length: 100 })
   first_name: string;
-  @Column({ type: 'varchar', length: 100 })
+  @Column({ type: 'varchar', length: 100, default: '' })
   middle_name: string;
   @Column({ type: 'varchar', length: 100 })
   last_name: string;
-  @Column({ type: 'varchar', length: 100 })
+  @Column({ type: 'varchar', length: 100, default: '' })
   second_last_name: string;
   @Column({ type: 'date' })
   birth_date: Date;
-  @Column({ type: 'date' })
+  @Column({ type: 'date', default: () => 'CURRENT_TIMESTAMP' })
   register_date: Date;
   @Column({ type: 'varchar', length: 100 })
   email: string;
-  @Column({ type: 'varchar', length: 20 })
+  @Column({ type: 'varchar' })
   phone_number: string;
   @Column({ type: 'varchar' })
   address: string;
