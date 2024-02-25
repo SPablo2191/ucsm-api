d6fec715568cf6841b87aedea04ee9b4ccd36c8f
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-24 23:14:25 -0300

    feat: add swagger dependency

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import 'reflect-metadata';
-dotenv.config();
+
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
+
+  const options = new DocumentBuilder()
+    .setTitle('Documentation | Best API')
+    .setDescription('The Best API documentation')
+    .setVersion('1.0')
+    .addApiKey({ type: 'apiKey', name: 'x-key', in: 'header' }, 'x-key')
+    .addBearerAuth({ type: 'http', scheme: 'bearer' }, 'authenticate')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
   });
