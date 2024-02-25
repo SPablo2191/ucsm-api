fa7f047ec2ed27f76380a36b77b5a42ed674b090
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-25 18:09:20 -0300

    feat: add student entity

--- src/modules/student/entities/student.entity.ts
+++ src/modules/student/entities/student.entity.ts
@@ -1,1 +1,29 @@
-export class Student {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Student {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+  @Column({ type: 'varchar', length: 100 })
+  first_name: string;
+  @Column({ type: 'varchar', length: 100 })
+  middle_name: string;
+  @Column({ type: 'varchar', length: 100 })
+  last_name: string;
+  @Column({ type: 'varchar', length: 100 })
+  second_last_name: string;
+  @Column({ type: 'date' })
+  birth_date: Date;
+  @Column({ type: 'date' })
+  register_date: Date;
+  @Column({ type: 'varchar', length: 100 })
+  email: string;
+  @Column({ type: 'varchar', length: 20 })
+  phone_number: string;
+  @Column({ type: 'varchar' })
+  address: string;
+  @Column({ type: 'varchar', length: 50 })
+  identification_document: string;
+  @Column({ type: 'varchar' })
+  password: string;
+}
