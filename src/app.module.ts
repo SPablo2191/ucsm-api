51aa61b25d3c6938760f469ecb1d106f2f2c3dac
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-26 01:22:04 -0300

    feat: add student module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConfigService } from './config/database-config/database-config.service';
 import * as dotenv from 'dotenv';
+import { StudentModule } from './modules/student/student.module';
 
 dotenv.config();
 const ENV = process.env.NODE_ENV;
@@ -19,6 +20,7 @@ const ENV = process.env.NODE_ENV;
       isGlobal: true,
       envFilePath: `.env.${ENV}`,
     }),
+    StudentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
