4cb6131275d9cf153a88189883d5690da65b4444
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-26 23:56:23 -0300

    feat: add installment entity

--- src/modules/installment/entities/installment.entity.ts
+++ src/modules/installment/entities/installment.entity.ts
@@ -1,1 +1,16 @@
-export class Installment {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Installment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  total_amount: number;
+  @Column({ type: 'date' })
+  register_date: Date;
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  paid_amount: number;
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  owed_amount: number;
+  @Column({ type: 'varchar' })
+  code: string;
+}
