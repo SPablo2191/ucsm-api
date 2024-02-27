ba135ef6dbbda8727f8620a56e8828383c963e3d
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-27 16:46:13 -0300

    feat: add debt entity

--- src/modules/debt/entities/debt.entity.ts
+++ src/modules/debt/entities/debt.entity.ts
@@ -1,1 +1,12 @@
-export class Debt {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Debt {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  balance: number;
+  @Column({ type: 'date' })
+  register_date: Date;
+  @Column({ type: 'date', default: () => 'CURRENT_TIMESTAMP' })
+  updated_date: Date;
+}
