ada6e786b1d75b328e375b3e5ec24665b381fd97
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-02-29 17:11:16 -0300

    feat: add installment repository

---
+++ src/modules/installment/installment.repository.spec.ts
@@ -0,0 +1,7 @@
+import { InstallmentRepository } from './installment.repository';
+
+describe('InstallmentRepository', () => {
+  it('should be defined', () => {
+    expect(new InstallmentRepository()).toBeDefined();
+  });
+});
---
+++ src/modules/installment/installment.repository.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Installment } from './entities/installment.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { InstallmentMapper } from './installment.mapper';
+import { Repository } from 'typeorm';
+import { BaseRepository } from 'src/core/classes/repository.base';
+
+@Injectable()
+export class InstallmentRepository implements BaseRepository<Installment> {
+  constructor(
+    @InjectRepository(Installment)
+    private installmentsRepository: Repository<Installment>,
+    private mapper: InstallmentMapper,
+  ) {}
+  getAll(): Promise<Installment[]> {
+    return this.installmentsRepository.find({
+      order: { register_date: 'DESC' },
+    });
+  }
+  create(item: Installment): Promise<Installment> {
+    const newInstallment = this.mapper.dtoToEntity(item);
+    return this.installmentsRepository.save(newInstallment);
+  }
+  getById(id: string): Promise<Installment> {
+    return this.installmentsRepository.findOne({ where: { id: id } });
+  }
+  update(id: string, item: Installment): Promise<Installment> {
+    item.id = id;
+    const updateInstallment = this.mapper.dtoToEntity(item);
+    return this.installmentsRepository.save(updateInstallment);
+  }
+  delete(id: string): Promise<Installment> {
+    return this.installmentsRepository
+      .findOne({ where: { id: id } })
+      .then((installment) => {
+        return this.installmentsRepository.remove(installment);
+      });
+  }
+}
