ead0bdaf3a37c5568a1903128318e912f11c5d19
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-02 14:46:09 -0300

    fix: controller and modify module

--- src/modules/building/building.module.ts
+++ src/modules/building/building.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BuildingService } from './building.service';
 import { BuildingController } from './building.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Building } from './entities/building.entity';
+import { BuildingMapper } from './building.mapper';
+import { BuildingRepository } from './building.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Building])],
   controllers: [BuildingController],
-  providers: [BuildingService],
+  providers: [BuildingService, BuildingMapper, BuildingRepository],
 })
 export class BuildingModule {}
