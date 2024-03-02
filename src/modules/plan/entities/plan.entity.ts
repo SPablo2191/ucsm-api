c0683dcf5a6b11258385264cb0ecc9f3bbdcbd4d
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-01 22:45:51 -0300

    feat: add plan entity

--- src/modules/plan/entities/plan.entity.ts
+++ src/modules/plan/entities/plan.entity.ts
@@ -1,1 +1,12 @@
-export class Plan {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Plan {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ type: 'date' })
+  start_year: Date;
+  @Column({ type: 'date' })
+  end_year: Date;
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+}
