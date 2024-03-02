522e432c5c5e9ae99beef83437373e64fac77976
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-02 17:22:22 -0300

    feat: add professor service

--- src/modules/professor/professor.service.ts
+++ src/modules/professor/professor.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProfessorDto } from './dto/create-professor.dto';
 import { UpdateProfessorDto } from './dto/update-professor.dto';
+import { BaseService } from 'src/core/services/base.interface.service';
+import { ProfessorRepository } from './professor.repository';
+import { ProfessorMapper } from './professor.mapper';
 
 @Injectable()
-export class ProfessorService {
-  create(createProfessorDto: CreateProfessorDto) {
-    return 'This action adds a new professor';
+export class ProfessorService
+  implements BaseService<CreateProfessorDto | UpdateProfessorDto>
+{
+  constructor(
+    private itemsRepository: ProfessorRepository,
+    private mapper: ProfessorMapper,
+  ) {}
+  findAll(): Promise<(CreateProfessorDto | UpdateProfessorDto)[]> {
+    return this.itemsRepository.getAll().then((items) => {
+      return items.map((record) => this.mapper.entityToDto(record));
+    });
   }
-
-  findAll() {
-    return `This action returns all professor`;
+  create(
+    item: CreateProfessorDto,
+  ): Promise<CreateProfessorDto | UpdateProfessorDto> {
+    return this.itemsRepository.create(item).then((newItem) => {
+      return this.mapper.entityToDto(newItem);
+    });
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} professor`;
+  findOne(id: string): Promise<CreateProfessorDto | UpdateProfessorDto> {
+    return this.itemsRepository.getById(id).then((item) => {
+      return this.mapper.entityToDto(item);
+    });
   }
-
-  update(id: number, updateProfessorDto: UpdateProfessorDto) {
-    return `This action updates a #${id} professor`;
+  update(
+    id: string,
+    item: CreateProfessorDto | UpdateProfessorDto,
+  ): Promise<CreateProfessorDto | UpdateProfessorDto> {
+    return this.itemsRepository.update(id, item).then((updateItem) => {
+      return this.mapper.entityToDto(updateItem);
+    });
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} professor`;
+  delete(id: string): Promise<CreateProfessorDto | UpdateProfessorDto> {
+    return this.itemsRepository.delete(id).then(() => {
+      return this.findOne(id);
+    });
   }
 }
