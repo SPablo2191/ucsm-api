95691635ad2d5c150e591199be6158c65d0df5ee
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-01 22:52:22 -0300

    feat: add plan dto

--- src/modules/plan/dto/create-plan.dto.ts
+++ src/modules/plan/dto/create-plan.dto.ts
@@ -1,1 +1,22 @@
-export class CreatePlanDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePlanDto {
+  @ApiProperty({ description: 'Start year of the plan', example: '2022-01-01' })
+  @IsDateString()
+  @IsNotEmpty()
+  start_year: Date;
+
+  @ApiProperty({ description: 'End year of the plan', example: '2023-12-31' })
+  @IsDateString()
+  @IsNotEmpty()
+  end_year: Date;
+
+  @ApiProperty({
+    description: 'Name of the plan',
+    example: '2022-2023 Academic Year',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
