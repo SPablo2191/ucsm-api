f5b995b1e9a3751604ab77aa78483a0e288f3fb3
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-02 16:44:48 -0300

    feat: add professor entity

--- src/modules/professor/entities/professor.entity.ts
+++ src/modules/professor/entities/professor.entity.ts
@@ -1,1 +1,19 @@
-export class Professor {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Professor {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ type: 'varchar', length: 100 })
+  first_name: string;
+  @Column({ type: 'varchar', length: 100, default: '' })
+  middle_name: string;
+  @Column({ type: 'varchar', length: 100 })
+  last_name: string;
+  @Column({ type: 'varchar', length: 100, default: '' })
+  second_last_name: string;
+  @Column({ type: 'varchar', length: 100 })
+  email: string;
+  @Column({ type: 'date', default: () => 'CURRENT_TIMESTAMP' })
+  register_date: Date;
+}
