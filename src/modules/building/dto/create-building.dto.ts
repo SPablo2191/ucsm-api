ea23d583a206815e77bf8e749cdbe6d757e9b02c
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-02 13:43:12 -0300

    feat: add building dto and entity

--- src/modules/building/dto/create-building.dto.ts
+++ src/modules/building/dto/create-building.dto.ts
@@ -1,1 +1,24 @@
-export class CreateBuildingDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateBuildingDto {
+  @ApiProperty({
+    description: 'ID of the building',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @ApiProperty({
+    description: 'Name of the building',
+    example: 'Pabellon L',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  constructor(partial: Partial<CreateBuildingDto>) {
+    Object.assign(this, partial);
+  }
+}
--- src/modules/building/entities/building.entity.ts
+++ src/modules/building/entities/building.entity.ts
@@ -1,1 +1,8 @@
-export class Building {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Building {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+}
