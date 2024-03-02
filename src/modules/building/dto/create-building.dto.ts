3f0237249fe38caa41c91d3809f531c2206fca28
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-02 13:45:17 -0300

    fix: change validator in building dto id

--- src/modules/building/dto/create-building.dto.ts
+++ src/modules/building/dto/create-building.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class CreateBuildingDto {
   @ApiProperty({
     description: 'ID of the building',
     example: '123e4567-e89b-12d3-a456-426614174000',
   })
-  @IsString()
+  @IsUUID()
   @IsNotEmpty()
   id: string;
 
