a77c5dae874464c98e3de4945f02dbfc24b35f04
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-03 17:10:52 -0300

    feat: add commission schedule entity

--- src/modules/commission-schedule/entities/commission-schedule.entity.ts
+++ src/modules/commission-schedule/entities/commission-schedule.entity.ts
@@ -1,1 +1,13 @@
-export class CommissionSchedule {}
+import { Days } from 'src/shared/types/days.types';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class CommissionSchedule {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ type: 'time' })
+  start_time: string;
+  @Column({ type: 'time' })
+  end_time: string;
+  @Column({ type: 'enum', enum: Days, default: Days.MONDAY })
+  day: string;
+}
