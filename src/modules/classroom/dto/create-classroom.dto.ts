013d2f0ae29df15b5bae53d6332d1410a9204ba4
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-03 00:04:55 -0300

    feat: add classroom dto

--- src/modules/classroom/dto/create-classroom.dto.ts
+++ src/modules/classroom/dto/create-classroom.dto.ts
@@ -1,1 +1,27 @@
-export class CreateClassroomDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { Building } from 'src/modules/building/entities/building.entity';
+
+export class CreateClassroomDto {
+  @ApiProperty({
+    description: 'ID of the Classroom',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @IsUUID()
+  id: string;
+  @ApiProperty({
+    description: 'Name of the building',
+    example: 'Pabellon L',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ description: 'Building associated with the classroom' })
+  @Type(() => Building)
+  building: Building;
+  constructor(partial: Partial<CreateClassroomDto>) {
+    Object.assign(this, partial);
+  }
+}
