8bdc0487261042b243a0d28a46b0c9d80114bf44
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-03 13:36:28 -0300

    feat: add semester service

--- src/modules/semester/semester.service.ts
+++ src/modules/semester/semester.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSemesterDto } from './dto/create-semester.dto';
 import { UpdateSemesterDto } from './dto/update-semester.dto';
+import { BaseService } from 'src/core/services/base.interface.service';
+import { SemesterRepository } from './semester.repository';
+import { SemesterMapper } from './semester.mapper';
+import { Semester } from './entities/semester.entity';
 
 @Injectable()
-export class SemesterService {
-  create(createSemesterDto: CreateSemesterDto) {
-    return 'This action adds a new semester';
+export class SemesterService
+  implements BaseService<CreateSemesterDto | UpdateSemesterDto>
+{
+  constructor(
+    private itemsRepository: SemesterRepository,
+    private mapper: SemesterMapper,
+  ) {}
+  async findAll(): Promise<(CreateSemesterDto | UpdateSemesterDto)[]> {
+    const items: Semester[] = await this.itemsRepository.getAll();
+    return items.map((record) => this.mapper.entityToDto(record));
   }
-
-  findAll() {
-    return `This action returns all semester`;
+  async create(
+    item: CreateSemesterDto,
+  ): Promise<CreateSemesterDto | UpdateSemesterDto> {
+    const newItem: Semester = await this.itemsRepository.create(item);
+    return this.mapper.entityToDto(newItem);
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} semester`;
+  async findOne(id: string): Promise<CreateSemesterDto | UpdateSemesterDto> {
+    const item: Semester = await this.itemsRepository.getById(id);
+    return this.mapper.entityToDto(item);
   }
-
-  update(id: number, updateSemesterDto: UpdateSemesterDto) {
-    return `This action updates a #${id} semester`;
+  async update(
+    id: string,
+    item: UpdateSemesterDto,
+  ): Promise<UpdateSemesterDto> {
+    const updateItem = await this.itemsRepository.update(id, item);
+    return this.mapper.entityToDto(updateItem);
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} semester`;
+  async delete(id: string): Promise<CreateSemesterDto | UpdateSemesterDto> {
+    await this.itemsRepository.delete(id);
+    return this.findOne(id);
   }
 }
