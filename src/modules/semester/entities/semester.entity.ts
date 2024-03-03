0f85770c103e9a489ae40b7ce8911230674ecec2
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-03 02:41:07 -0300

    feat: add semester resource

---
+++ src/modules/semester/entities/semester.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Semester {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+  @Column({ type: 'date' })
+  start_date: Date;
+  @Column({ type: 'date' })
+  end_date: Date;
+}
