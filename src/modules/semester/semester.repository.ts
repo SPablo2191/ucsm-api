672f42898a3a364bee47db0653b0233ece31139f
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-03 13:13:22 -0300

    feat: add semester repository

--- src/modules/semester/semester.repository.ts
+++ src/modules/semester/semester.repository.ts
@@ -1,10 +1,36 @@
 import { BaseRepository } from 'src/core/classes/repository.base';
 import { Semester } from './entities/semester.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SemesterMapper } from './semester.mapper';
+import { Repository } from 'typeorm';
+import { UpdateSemesterDto } from './dto/update-semester.dto';
 
-export class SemesterRepository implements BaseRepository<Semester> {
-  getAll: () => Promise<Semester[]>;
-  create: (item: Semester) => Promise<Semester>;
-  getById: (id: string) => Promise<Semester>;
-  update: (id: string, item: Semester) => Promise<Semester>;
-  delete: (id: string) => Promise<Semester>;
+export class SemesterRepository
+  implements BaseRepository<Semester | UpdateSemesterDto>
+{
+  constructor(
+    @InjectRepository(Semester)
+    private itemsRepository: Repository<Semester>,
+    private mapper: SemesterMapper,
+  ) {}
+  getAll(): Promise<Semester[]> {
+    return this.itemsRepository.find();
+  }
+  create(item: Semester): Promise<Semester> {
+    const newSemester = this.mapper.dtoToEntity(item);
+    return this.itemsRepository.save(newSemester);
+  }
+  getById(id: string): Promise<Semester> {
+    return this.itemsRepository.findOne({ where: { id: id } });
+  }
+  update(id: string, item: UpdateSemesterDto | Semester): Promise<Semester> {
+    item.id = id;
+    const updatePlan = this.mapper.dtoToEntity(item);
+    return this.itemsRepository.save(updatePlan);
+  }
+  delete(id: string): Promise<Semester> {
+    return this.itemsRepository.findOne({ where: { id: id } }).then((debt) => {
+      return this.itemsRepository.remove(debt);
+    });
+  }
 }
