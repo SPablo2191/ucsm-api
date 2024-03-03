38d0a434e4687f6ce764af13f0136e139b984d1e
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-03 00:55:31 -0300

    feat: add controller and module

--- src/modules/classroom/classroom.module.ts
+++ src/modules/classroom/classroom.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ClassroomService } from './classroom.service';
 import { ClassroomController } from './classroom.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Classroom } from './entities/classroom.entity';
+import { ClassroomMapper } from './classroom.mapper';
+import { ClassroomRepository } from './classroom.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Classroom])],
   controllers: [ClassroomController],
-  providers: [ClassroomService],
+  providers: [ClassroomService, ClassroomMapper, ClassroomRepository],
 })
 export class ClassroomModule {}
