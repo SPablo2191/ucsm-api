f6eef5d88096d83f3a13dce11339ea567ced7a57
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-05 22:26:40 -0300

    feat: add commission schedule service

--- src/modules/commission-schedule/commission-schedule.service.ts
+++ src/modules/commission-schedule/commission-schedule.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommissionScheduleDto } from './dto/create-commission-schedule.dto';
 import { UpdateCommissionScheduleDto } from './dto/update-commission-schedule.dto';
+import { BaseService } from 'src/core/services/base.interface.service';
+import { CommissionScheduleMapper } from './commission-schedule.mapper';
+import { CommissionScheduleRepository } from './commission-schedule.repository';
+import { CommissionSchedule } from './entities/commission-schedule.entity';
 
 @Injectable()
-export class CommissionScheduleService {
-  create(createCommissionScheduleDto: CreateCommissionScheduleDto) {
-    return 'This action adds a new commissionSchedule';
+export class CommissionScheduleService
+  implements
+    BaseService<CreateCommissionScheduleDto | UpdateCommissionScheduleDto>
+{
+  constructor(
+    private itemsRepository: CommissionScheduleRepository,
+    private mapper: CommissionScheduleMapper,
+  ) {}
+  async findAll(): Promise<
+    (CreateCommissionScheduleDto | UpdateCommissionScheduleDto)[]
+  > {
+    const items: CommissionSchedule[] = await this.itemsRepository.getAll();
+    return items.map((record) => this.mapper.entityToDto(record));
   }
-
-  findAll() {
-    return `This action returns all commissionSchedule`;
+  async create(
+    item: CreateCommissionScheduleDto,
+  ): Promise<CreateCommissionScheduleDto | UpdateCommissionScheduleDto> {
+    const newItem: CommissionSchedule = await this.itemsRepository.create(item);
+    return this.mapper.entityToDto(newItem);
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} commissionSchedule`;
+  async findOne(
+    id: string,
+  ): Promise<CreateCommissionScheduleDto | UpdateCommissionScheduleDto> {
+    const item: CommissionSchedule = await this.itemsRepository.getById(id);
+    return this.mapper.entityToDto(item);
   }
-
-  update(id: number, updateCommissionScheduleDto: UpdateCommissionScheduleDto) {
-    return `This action updates a #${id} commissionSchedule`;
+  async update(
+    id: string,
+    item: UpdateCommissionScheduleDto,
+  ): Promise<UpdateCommissionScheduleDto> {
+    const updateItem = await this.itemsRepository.update(id, item);
+    return this.mapper.entityToDto(updateItem);
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} commissionSchedule`;
+  async delete(
+    id: string,
+  ): Promise<CreateCommissionScheduleDto | UpdateCommissionScheduleDto> {
+    await this.itemsRepository.delete(id);
+    return this.findOne(id);
   }
 }
