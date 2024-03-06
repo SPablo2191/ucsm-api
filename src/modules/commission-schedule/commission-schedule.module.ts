069c4e6d1ecc5605b9fb655e69db0884314adcdd
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-05 22:30:33 -0300

    feat: add modules in both resources

--- src/modules/commission-schedule/commission-schedule.module.ts
+++ src/modules/commission-schedule/commission-schedule.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CommissionScheduleService } from './commission-schedule.service';
 import { CommissionScheduleController } from './commission-schedule.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommissionSchedule } from './entities/commission-schedule.entity';
+import { CommissionScheduleMapper } from './commission-schedule.mapper';
+import { CommissionScheduleRepository } from './commission-schedule.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CommissionSchedule])],
   controllers: [CommissionScheduleController],
-  providers: [CommissionScheduleService],
+  providers: [
+    CommissionScheduleService,
+    CommissionScheduleMapper,
+    CommissionScheduleRepository,
+  ],
 })
 export class CommissionScheduleModule {}
--- src/modules/commission/commission.module.ts
+++ src/modules/commission/commission.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CommissionService } from './commission.service';
 import { CommissionController } from './commission.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Commission } from './entities/commission.entity';
+import { CommissionMapper } from './commission.mapper';
+import { CommissionRepository } from './commission.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Commission])],
   controllers: [CommissionController],
-  providers: [CommissionService],
+  providers: [CommissionService, CommissionMapper, CommissionRepository],
 })
 export class CommissionModule {}
