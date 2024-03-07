37c03eac73dd1dd9d098eefe938e4177d11e8097
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-07 12:02:36 -0300

    feat: add imports and providers in academic program

--- src/modules/academic-program/academic-program.module.ts
+++ src/modules/academic-program/academic-program.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AcademicProgramService } from './academic-program.service';
 import { AcademicProgramController } from './academic-program.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AcademicProgram } from './entities/academic-program.entity';
+import { AcademicProgramMapper } from './academic-program.mapper';
+import { AcademicProgramRepository } from './academic-program.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AcademicProgram])],
   controllers: [AcademicProgramController],
-  providers: [AcademicProgramService],
+  providers: [
+    AcademicProgramService,
+    AcademicProgramMapper,
+    AcademicProgramRepository,
+  ],
 })
 export class AcademicProgramModule {}
