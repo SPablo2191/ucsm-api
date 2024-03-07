546ea4633ddd15959620cdae1ed262ae2b97e9a0
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-07 11:55:37 -0300

    feat: add academic program service

--- src/modules/academic-program/academic-program.service.ts
+++ src/modules/academic-program/academic-program.service.ts
@@ -1,26 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAcademicProgramDto } from './dto/create-academic-program.dto';
 import { UpdateAcademicProgramDto } from './dto/update-academic-program.dto';
+import { BaseService } from 'src/core/services/base.interface.service';
+import { AcademicProgramRepository } from './academic-program.repository';
+import { AcademicProgramMapper } from './academic-program.mapper';
+import { AcademicProgram } from './entities/academic-program.entity';
 
 @Injectable()
-export class AcademicProgramService {
-  create(createAcademicProgramDto: CreateAcademicProgramDto) {
-    return 'This action adds a new academicProgram';
+export class AcademicProgramService
+  implements BaseService<CreateAcademicProgramDto | UpdateAcademicProgramDto>
+{
+  constructor(
+    private itemsRepository: AcademicProgramRepository,
+    private mapper: AcademicProgramMapper,
+  ) {}
+  async findAll(): Promise<
+    (CreateAcademicProgramDto | UpdateAcademicProgramDto)[]
+  > {
+    const items: AcademicProgram[] = await this.itemsRepository.getAll();
+    return items.map((record) => this.mapper.entityToDto(record));
   }
-
-  findAll() {
-    return `This action returns all academicProgram`;
+  async create(
+    item: CreateAcademicProgramDto,
+  ): Promise<CreateAcademicProgramDto | UpdateAcademicProgramDto> {
+    const newItem: AcademicProgram = await this.itemsRepository.create(item);
+    return this.mapper.entityToDto(newItem);
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} academicProgram`;
+  async findOne(
+    id: string,
+  ): Promise<CreateAcademicProgramDto | UpdateAcademicProgramDto> {
+    const item: AcademicProgram = await this.itemsRepository.getById(id);
+    return this.mapper.entityToDto(item);
   }
-
-  update(id: number, updateAcademicProgramDto: UpdateAcademicProgramDto) {
-    return `This action updates a #${id} academicProgram`;
+  async update(
+    id: string,
+    item: UpdateAcademicProgramDto,
+  ): Promise<UpdateAcademicProgramDto> {
+    const updateItem = await this.itemsRepository.update(id, item);
+    return this.mapper.entityToDto(updateItem);
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} academicProgram`;
+  async delete(
+    id: string,
+  ): Promise<CreateAcademicProgramDto | UpdateAcademicProgramDto> {
+    await this.itemsRepository.delete(id);
+    return this.findOne(id);
   }
 }
