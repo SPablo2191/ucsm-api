eeb293bef2ce1712230d347e4ae90c62f325f67f
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-10 11:57:18 -0300

    feat: add enrollment dto

--- src/modules/enrollment/dto/create-enrollment.dto.ts
+++ src/modules/enrollment/dto/create-enrollment.dto.ts
@@ -1,1 +1,40 @@
-export class CreateEnrollmentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsUUID,
+  ValidateNested,
+} from 'class-validator';
+import { AcademicProgram } from 'src/modules/academic-program/entities/academic-program.entity';
+import { Student } from 'src/modules/student/entities/student.entity';
+
+export class CreateEnrollmentDto {
+  @ApiProperty({ description: 'ID of enrollment' })
+  @IsUUID()
+  id: string;
+
+  @ApiProperty({ description: 'Enrollment associate to subject registrations' })
+  @Type(() => AcademicProgram)
+  @ValidateNested()
+  academic_program: AcademicProgram;
+
+  @ApiProperty({ description: 'Student associated with the enrollment' })
+  @Type(() => Student)
+  student: Student;
+
+  @ApiProperty({ description: 'Enrollment Code', maxLength: 50 })
+  @IsNotEmpty()
+  code: string;
+
+  @ApiProperty({ description: 'Registration Date' })
+  @IsDateString()
+  register_date: Date;
+
+  @ApiProperty({ description: 'Update Date' })
+  @IsDateString()
+  update_date: Date;
+  constructor(partial: Partial<CreateEnrollmentDto>) {
+    Object.assign(this, partial);
+  }
+}
