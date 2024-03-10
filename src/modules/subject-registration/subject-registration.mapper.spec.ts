b246cfcf583b657835a9918c1d96a932de9173ca
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-10 17:47:39 -0300

    feat: add subject registration mapper

---
+++ src/modules/subject-registration/subject-registration.mapper.spec.ts
@@ -0,0 +1,7 @@
+import { SubjectRegistrationMapper } from './subject-registration.mapper';
+
+describe('SubjectRegistrationMapper', () => {
+  it('should be defined', () => {
+    expect(new SubjectRegistrationMapper()).toBeDefined();
+  });
+});
