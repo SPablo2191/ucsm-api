ae9b3cd5f4b81c04393ac5883fe180661fea0809
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-10 00:27:02 -0300

    feat: add service

--- src/modules/subject/subject.service.ts
+++ src/modules/subject/subject.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSubjectDto } from './dto/create-subject.dto';
 import { UpdateSubjectDto } from './dto/update-subject.dto';
+import { BaseService } from 'src/core/services/base.interface.service';
+import { Subject } from './entities/subject.entity';
+import { SubjectRepository } from './subject.repository';
+import { SubjectMapper } from './subject.mapper';
 
 @Injectable()
-export class SubjectService {
-  create(createSubjectDto: CreateSubjectDto) {
-    return 'This action adds a new subject';
+export class SubjectService
+  implements BaseService<CreateSubjectDto | UpdateSubjectDto>
+{
+  constructor(
+    private itemsRepository: SubjectRepository,
+    private mapper: SubjectMapper,
+  ) {}
+  async findAll(): Promise<(CreateSubjectDto | UpdateSubjectDto)[]> {
+    const items: Subject[] = await this.itemsRepository.getAll();
+    return items.map((record) => this.mapper.entityToDto(record));
   }
-
-  findAll() {
-    return `This action returns all subject`;
+  async create(
+    item: CreateSubjectDto,
+  ): Promise<CreateSubjectDto | UpdateSubjectDto> {
+    const newItem: Subject = await this.itemsRepository.create(item);
+    return this.mapper.entityToDto(newItem);
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} subject`;
+  async findOne(id: string): Promise<CreateSubjectDto | UpdateSubjectDto> {
+    const item: Subject = await this.itemsRepository.getById(id);
+    return this.mapper.entityToDto(item);
   }
-
-  update(id: number, updateSubjectDto: UpdateSubjectDto) {
-    return `This action updates a #${id} subject`;
+  async update(id: string, item: UpdateSubjectDto): Promise<UpdateSubjectDto> {
+    const updateItem = await this.itemsRepository.update(id, item);
+    return this.mapper.entityToDto(updateItem);
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} subject`;
+  async delete(id: string): Promise<CreateSubjectDto | UpdateSubjectDto> {
+    await this.itemsRepository.delete(id);
+    return this.findOne(id);
   }
 }
