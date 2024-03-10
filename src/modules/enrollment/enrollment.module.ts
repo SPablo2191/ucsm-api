d2b8323a618ec529cb587548e52056fe2f3abdac
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-10 17:30:27 -0300

    feat: add enrollment module

--- src/modules/enrollment/enrollment.module.ts
+++ src/modules/enrollment/enrollment.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EnrollmentService } from './enrollment.service';
 import { EnrollmentController } from './enrollment.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Enrollment } from './entities/enrollment.entity';
+import { EnrollmentRepository } from './enrollment.repository';
+import { EnrollmentMapper } from './enrollment.mapper';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Enrollment])],
   controllers: [EnrollmentController],
-  providers: [EnrollmentService],
+  providers: [EnrollmentService, EnrollmentMapper, EnrollmentRepository],
 })
 export class EnrollmentModule {}
