0d20f9cfcdce407d918eb58b94c03b439c7e79ec
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-10 00:27:14 -0300

    feat: add subject module

--- src/modules/subject/subject.module.ts
+++ src/modules/subject/subject.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SubjectService } from './subject.service';
 import { SubjectController } from './subject.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Subject } from './entities/subject.entity';
+import { SubjectMapper } from './subject.mapper';
+import { SubjectRepository } from './subject.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Subject])],
   controllers: [SubjectController],
-  providers: [SubjectService],
+  providers: [SubjectService, SubjectMapper, SubjectRepository],
 })
 export class SubjectModule {}
