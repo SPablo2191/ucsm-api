18ad2effdca50dfbcec388b911353130dc55fa37
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-10 17:47:16 -0300

    feat: add subject registration repository

---
+++ src/modules/subject-registration/subject-registration.repository.spec.ts
@@ -0,0 +1,7 @@
+import { SubjectRegistrationRepository } from './subject-registration.repository';
+
+describe('SubjectRegistrationRepository', () => {
+  it('should be defined', () => {
+    expect(new SubjectRegistrationRepository()).toBeDefined();
+  });
+});
