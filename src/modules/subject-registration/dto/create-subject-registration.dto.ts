7786c205084a1911efc9a91a184c848c24ba1f78
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-10 01:11:43 -0300

    feat: add subject registration dto

--- src/modules/subject-registration/dto/create-subject-registration.dto.ts
+++ src/modules/subject-registration/dto/create-subject-registration.dto.ts
@@ -1,1 +1,48 @@
-export class CreateSubjectRegistrationDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsDateString,
+  IsOptional,
+  IsUUID,
+  ValidateNested,
+} from 'class-validator';
+import { Subject } from 'src/modules/subject/entities/subject.entity';
+
+export class CreateSubjectRegistrationDto {
+  @ApiProperty({ description: 'ID of the subject registration' })
+  @IsUUID()
+  id: string;
+
+  @ApiProperty({ description: 'Subject associate to subject registrations' })
+  @Type(() => Subject)
+  @ValidateNested()
+  academic_program: Subject;
+  @ApiProperty({
+    description: 'Final score of the subject registration',
+    required: false,
+  })
+  @IsOptional()
+  final_score: number;
+
+  @ApiProperty({
+    description: 'Simulated score of the subject registration',
+    required: false,
+  })
+  @IsOptional()
+  simulated_score: number;
+
+  @ApiProperty({
+    description: 'Date when the subject registration was created',
+  })
+  @IsDateString()
+  register_date: Date;
+
+  @ApiProperty({
+    description: 'Date when the subject registration was last updated',
+  })
+  @IsDateString()
+  update_date: Date;
+  constructor(partial: Partial<CreateSubjectRegistrationDto>) {
+    Object.assign(this, partial);
+  }
+}
