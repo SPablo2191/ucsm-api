27a425dde3cbec893cfe19d4bb1c764c885e8478
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-09 21:59:47 -0300

    feat: add subject dto

--- src/modules/subject/dto/create-subject.dto.ts
+++ src/modules/subject/dto/create-subject.dto.ts
@@ -1,1 +1,32 @@
-export class CreateSubjectDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsUUID, ValidateNested } from 'class-validator';
+import { AcademicProgram } from 'src/modules/academic-program/entities/academic-program.entity';
+import { Semester } from 'src/modules/semester/entities/semester.entity';
+
+export class CreateSubjectDto {
+  @ApiProperty({
+    description: 'ID of the Subject',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @IsUUID()
+  id: string;
+  @ApiProperty({
+    description: 'Name of the Subject',
+    example: 'Human Resources',
+  })
+  @IsNotEmpty()
+  name: string;
+  @ApiProperty({ description: 'Academic Program associate to subject' })
+  @Type(() => AcademicProgram)
+  @ValidateNested()
+  academic_program: AcademicProgram;
+
+  @ApiProperty({ description: 'Semester associate to subject' })
+  @Type(() => Semester)
+  @ValidateNested()
+  semester: Semester;
+  constructor(partial: Partial<CreateSubjectDto>) {
+    Object.assign(this, partial);
+  }
+}
