5c92f1f7ba2fbefe38c926fe8ab8a03d209cbf80
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-12 01:25:33 -0300

    feat: add student commission dto

--- src/modules/student-commission/dto/create-student-commission.dto.ts
+++ src/modules/student-commission/dto/create-student-commission.dto.ts
@@ -1,1 +1,25 @@
-export class CreateStudentCommissionDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsUUID } from 'class-validator';
+import { Commission } from 'src/modules/commission/entities/commission.entity';
+import { SubjectRegistration } from 'src/modules/subject-registration/entities/subject-registration.entity';
+
+export class CreateStudentCommissionDto {
+  @ApiProperty({
+    description: 'ID of the installment',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @IsUUID()
+  id: string;
+  @ApiProperty({ description: 'Commission associated with the isntallment' })
+  @Type(() => Commission)
+  commission: Commission;
+  @ApiProperty({
+    description: 'SubjectRegistration associated with the isntallment',
+  })
+  @Type(() => SubjectRegistration)
+  subject_registration: SubjectRegistration;
+  constructor(partial: Partial<CreateStudentCommissionDto>) {
+    Object.assign(this, partial);
+  }
+}
--- src/modules/student-commission/entities/student-commission.entity.ts
+++ src/modules/student-commission/entities/student-commission.entity.ts
@@ -1,8 +1,10 @@
 import { Commission } from 'src/modules/commission/entities/commission.entity';
 import { SubjectRegistration } from 'src/modules/subject-registration/entities/subject-registration.entity';
-import { Entity, ManyToOne } from 'typeorm';
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class StudentCommission {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
   @ManyToOne(() => Commission, (commission) => commission.student_commissions)
   commission: Commission;
   @ManyToOne(
