c2514ae3243a14dc5ab334935eb1bd302bb7cbde
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-12 01:51:31 -0300

    feat: add grade

--- src/modules/grade/entities/grade.entity.ts
+++ src/modules/grade/entities/grade.entity.ts
@@ -1,1 +1,21 @@
-export class Grade {}
+import { StudentCommission } from 'src/modules/student-commission/entities/student-commission.entity';
+import { Phase } from 'src/shared/types/phase.types';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Grade {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @ManyToOne(
+    () => StudentCommission,
+    (commission) => commission.student_commissions,
+  )
+  student_commission: StudentCommission;
+  @Column({ type: 'decimal', precision: 5, scale: 2, nullable: true })
+  final_score: number;
+  @Column({ type: 'enum', enum: Phase, default: Phase.FIRST })
+  phase: string;
+  constructor(partial: Partial<Grade>) {
+    Object.assign(this, partial);
+  }
+}
--- src/modules/student-commission/entities/student-commission.entity.ts
+++ src/modules/student-commission/entities/student-commission.entity.ts
@@ -1,6 +1,7 @@
 import { Commission } from 'src/modules/commission/entities/commission.entity';
+import { Grade } from 'src/modules/grade/entities/grade.entity';
 import { SubjectRegistration } from 'src/modules/subject-registration/entities/subject-registration.entity';
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class StudentCommission {
   @PrimaryGeneratedColumn('uuid')
@@ -12,6 +13,8 @@ export class StudentCommission {
     (commission) => commission.student_commissions,
   )
   subject_registration: SubjectRegistration;
+  @OneToMany(() => Grade, (grade) => grade.student_commission)
+  student_commissions: Grade[];
   constructor(partial: Partial<StudentCommission>) {
     Object.assign(this, partial);
   }
