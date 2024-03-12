780332750f792db87e0f021df14a62556243a163
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-03-12 01:00:13 -0300

    feat: add subject registration mapper

--- src/modules/subject-registration/subject-registration.mapper.ts
+++ src/modules/subject-registration/subject-registration.mapper.ts
@@ -1,1 +1,21 @@
-export class SubjectRegistrationMapper {}
+import { Injectable } from '@nestjs/common';
+import { CreateSubjectRegistrationDto } from './dto/create-subject-registration.dto';
+import { UpdateSubjectRegistrationDto } from './dto/update-subject-registration.dto';
+import { SubjectRegistration } from './entities/subject-registration.entity';
+
+@Injectable()
+export class SubjectRegistrationMapper {
+  dtoToEntity(
+    subjectRegistrationDTO:
+      | CreateSubjectRegistrationDto
+      | UpdateSubjectRegistrationDto,
+  ): SubjectRegistration {
+    return new SubjectRegistration(subjectRegistrationDTO);
+  }
+
+  entityToDto(
+    subjectRegistrationEntity: SubjectRegistration,
+  ): CreateSubjectRegistrationDto | UpdateSubjectRegistrationDto {
+    return new CreateSubjectRegistrationDto(subjectRegistrationEntity);
+  }
+}
